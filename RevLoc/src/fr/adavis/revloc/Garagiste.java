4bb5d598ae1d727e6a104c5788094d5c90427936
Driss <driss.belaroussi.sio@gmail.com>
2016-11-28 16:44:15 +0100

    Constructeur Garagiste

--- RevLoc/src/fr/adavis/revloc/Garagiste.java
+++ RevLoc/src/fr/adavis/revloc/Garagiste.java
@@ -5,7 +5,10 @@ public class Garagiste {
 	private String nom ;
 	private String prenom ;
 	
-	
+	public Garagiste (String nom , String prenom){
+		this.nom = nom ;
+		this.prenom = prenom ;
+	}
 	
 		
 	
