1aff21483f64388447349e1630a2aefc53efa384
Driss <driss.belaroussi.sio@gmail.com>
2016-11-28 16:51:07 +0100

    Getters et Setters

--- RevLoc/src/fr/adavis/revloc/Garagiste.java
+++ RevLoc/src/fr/adavis/revloc/Garagiste.java
@@ -8,6 +8,25 @@ public class Garagiste {
 	public Garagiste (String nom , String prenom){
 		this.nom = nom ;
 		this.prenom = prenom ;
+		
+		
+		
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
 	}
 	
 		
